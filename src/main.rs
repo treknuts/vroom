25b2e6e1570cb64a23ac1b203105dfe2afb8eca4
treknuts <trevor.a.knutson@ndsu.edu>
2023-10-09 20:27:03 -0500

    Attempting to do TCP connection handling with the standard net crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::{http::StatusCode, response::Json, routing::get, Router};
 use serde::Serialize;
-use std::net::SocketAddr;
+use std::io::prelude::*;
+use std::net::{SocketAddr, TcpListener, TcpStream};
 use uuid::Uuid;
 
 #[derive(Serialize)]
@@ -8,28 +9,43 @@ struct Chat {
     id: Uuid,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), StatusCode> {
-    let app = Router::new()
-        .route("/", get(health_check))
-        .route("/chat", get(create_chat));
+//    Basic Axum stuff. Might just build TCP stuff from scratch
+//    let app = Router::new()
+//        .route("/", get(health_check))
+//        .route("/chat", get(create_chat));
+//
+//    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
+//
+//    axum::Server::bind(&addr)
+//        .serve(app.into_make_service())
+//        .await
+//        .unwrap();
+//
+//    Ok(())
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    stream.write(&[1])?;
+    stream.read(&mut [0; 128])?;
     Ok(())
 }
 
-async fn health_check() -> &'static str {
-    "Server is healthy and running :)"
+// #[tokio::main]
+// async
+fn main() -> std::io::Result<()> {
+    let socket = TcpListener::bind("127.0.0.1:80")?;
+
+    for stream in socket.incoming() {
+        let _ = handle_client(stream?);
+    }
+    Ok(())
 }
 
-#[axum::debug_handler]
-async fn create_chat() -> Result<Json<Chat>, StatusCode> {
-    let chat = Chat { id: Uuid::new_v4() };
-    Ok(Json(chat))
-}
+// async fn health_check() -> &'static str {
+//     "Server is healthy and running :)"
+// }
+
+// #[axum::debug_handler]
+// async fn create_chat() -> Result<Json<Chat>, StatusCode> {
+//     let chat = Chat { id: Uuid::new_v4() };
+//     Ok(Json(chat))
+// }
